754723df7a54e3216e2fd067c547aca245fed9b1
Goh Sheen An <sheen.an.goh@gmail.com>
2020-03-17 16:20:17 +0800

    Updates

---
+++ precache-manifest.e03b2e4d6fd4fe2338cb5098fe95656a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c13287f3b04353c8f4998fcd646f9dd",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "a0f388242809a8befd71",
+    "url": "/react-pokedex/static/css/main.6769414c.chunk.css"
+  },
+  {
+    "revision": "fc46744cef2d678e13de",
+    "url": "/react-pokedex/static/js/2.1074eae8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-pokedex/static/js/2.1074eae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0f388242809a8befd71",
+    "url": "/react-pokedex/static/js/main.af0e9cd8.chunk.js"
+  },
+  {
+    "revision": "183b0e8cce640489db71",
+    "url": "/react-pokedex/static/js/runtime-main.89b4239f.js"
+  }
+]);
